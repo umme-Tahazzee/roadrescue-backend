08639cab9481e3da14cca7c61f0af2bf48d3e866
umme-Tahazzee <tahazzeekaria1@gmail.com>
2026-09-26 10:55:43 -0400

    fix;resolve building issues

--- src/app.ts
+++ src/app.ts
@@ -22,6 +22,11 @@ app.use(express.urlencoded({ extended: true }));
 // Middleware to parse JSON bodies
 app.use(express.json());
 app.use(cookieParser());
+app.get("/", (req:Request,res:Response)=>{
+      res.send("hello world")
+})
+
+
 app.use("/api/v1", router);
 
 app.use(notFound)
